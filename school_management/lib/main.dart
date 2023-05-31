9e67e3ac14605915af914ea4dcde09e875159d62
samwilson0745 <sohamdeb0745@gmail.com>
2023-05-31 21:39:37 +0530

    added timer to splashscreen

--- school_management/lib/main.dart
+++ school_management/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:school_management/routes/landing_page.dart';
+import 'package:school_management/routes/splashscreen.dart';
 import 'package:school_management/routes/schoolCode_page.dart';
 void main() {
   runApp(const MyApp());
